3c2479d4477811783e9c746dd3e8f238a1f54151
kaaposc <kaaposc@gmail.com>
2013-02-08 14:11:32 +0200

    first test green, added second test

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -2,7 +2,7 @@ var FizzBuzz = function (count) {
 	this.count = count;
 
 	this.print = function () {
-		
+		return [1];
 	}
 
 	return this;
@@ -14,6 +14,10 @@ describe('FizzBuzz', function() {
 		it('should return [1] when count is 1', function() {
 			fizzbuzz = new FizzBuzz(1);
 			fizzbuzz.print().should.eql([1]);
+		});
+		it('should return [1, 2, fizz] when count is 3', function() {
+			fizzbuzz = new FizzBuzz(3);
+			fizzbuzz.print().should.eql([1, 2, 'fizz']);
 		});
 	});
 });
